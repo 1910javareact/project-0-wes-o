7d33c0810851496e3dd91fc10178fb6e199c0f7c
wes-o <weso.turn@gmail.com>
2019-12-02 13:51:46 -0600

    changes

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -4,18 +4,21 @@ export function authorization(authRoles:string[]){
 //middleware function generated using authRoles parameter
     return (req,res,next)=>{
         let isAuth = false
+        
         //Check login
         if(!req.session.user){
             console.log(req.session.user + " 888");
             res.status(401).send('Please Login');
             return
         }
-        for(let userRole of req.session.user){
-            if(authRoles.includes(userRole)){
+        
+        //for(let userRole of req.session.user.role){
+        //let userRole = req.session.user.role  
+            if(authRoles.includes(req.session.user.role)){
                 isAuth = true
-                res.status(400).send("Thanks for logging in: " + userRole);
+                res.status(400).send("Thanks for logging in: " + req.session.user.role);
             }
-        }
+        
         if(isAuth){
             next()
         }else{
