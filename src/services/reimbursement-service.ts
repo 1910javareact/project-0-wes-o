49d53dffad13dde87eee53ade82a32da36003d27
wes-o <weso.turn@gmail.com>
2019-12-04 01:20:57 -0600

    updates to services

--- src/services/reimbursement-service.ts
+++ src/services/reimbursement-service.ts
@@ -2,41 +2,29 @@
 import { Reimbursement } from '../models/reimbursement';
 import { daoGetReimbursementsByStatusId, daoGetReimbursementsByUserId, daoSaveOneReimbursement } from '../repositories/reimbursement-dao';
 
-// export async function postReimbursements(post){ //:Reimbursement):Promise<Reimbursement>{
-//     //do some processing
-//     try{
-//     let res = await daoPostReimbursement(post)
-//     console.log(res + " -RES");
-//     return res
-//     }catch(e){
-//         throw e
-//     }    
-// }
 
-export async function getReimbursementsByStatusId(statusid:number):Promise<Reimbursement[]>{
-    console.log("Service: searching for reimbursement " + statusid + " ...");  
-    try{
+export async function getReimbursementsByStatusId(statusid: number): Promise<Reimbursement[]> {
+    console.log("Service: searching for reimbursement " + statusid + " ...");
+    try {
         return await daoGetReimbursementsByStatusId(statusid)
-    }catch(e){
+    } catch (e) {
         throw e
-    }    
+    }
 }
 
-export async function getReimbursementsByUserId(userid:number):Promise<Reimbursement[]>{
-    console.log("Service: searching for reimbursement " + userid + " ...");  
-    try{
+export async function getReimbursementsByUserId(userid: number): Promise<Reimbursement[]> {
+    console.log("Service: searching for reimbursement " + userid + " ...");
+    try {
         return await daoGetReimbursementsByUserId(userid)
-    }catch(e){
+    } catch (e) {
         throw e
-    }    
+    }
 }
 
-export async function saveOneReimbursement(reimbursement:Reimbursement):Promise<Reimbursement>{
-    try{
-        // console.log(reimbursement);
-        
+export async function saveOneReimbursement(reimbursement: Reimbursement): Promise<Reimbursement> {
+    try {
         return await daoSaveOneReimbursement(reimbursement)
-    }catch(e){
+    } catch (e) {
         throw e
     }
 }
